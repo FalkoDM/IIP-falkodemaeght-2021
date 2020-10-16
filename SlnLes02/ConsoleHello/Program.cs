660f5cf14b78c3b89b423396ec67c86e4092fdae
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-16 17:30:29 +0200

    console oefeningen

---
+++ SlnLes02/ConsoleHello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Wat is je naam? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Aangename kennismaking " + name);
+            Console.WriteLine("Druk op een toets om door te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
