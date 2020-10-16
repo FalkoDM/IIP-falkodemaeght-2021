eaac772f499057fa94a10b032271a126a59f99e4
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-16 15:19:45 +0200

    Eerste demo nabebouwd

---
+++ SlnLes02/ConsoleDemo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom!");
+            Console.Write("Wat is je naam?");
+            string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Welkom " + name);
+            Console.WriteLine("Druk op een toets om door te gaan...");
+            Console.ReadKey();
+        }
+    }
+}
