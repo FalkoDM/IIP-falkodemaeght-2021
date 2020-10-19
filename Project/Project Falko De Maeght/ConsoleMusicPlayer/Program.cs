517b092314aa686547f6fb3172ff3e4eda3f290f
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-19 15:08:10 +0200

    Project

---
+++ Project/Project Falko De Maeght/ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace ConsoleMusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+        }
+    }
+}
