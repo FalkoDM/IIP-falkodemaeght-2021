9db8d0e568030cabe5588cef6874eeb63bad80c6
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-19 16:53:32 +0200

    Wpf Pizza

--- SlnLes02/WpfPizza/WpfPizza/MainWindow.xaml.cs
+++ SlnLes02/WpfPizza/WpfPizza/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,32 @@ namespace WpfPizza
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rbn1_1_Checked(object sender, RoutedEventArgs e)
+        {
+            // beschrijving + foto als eerste box aangevinkt wordt
+            txtDescription.Text = "Tomatensaus, mozzarella, rode ui, Pepperoni en rundsvlees";
+            ImgPizza.Source = new BitmapImage(new Uri("Images/Hotspicy.jpg", UriKind.Relative));
+        }
+
+        private void rbn1_2_Checked(object sender, RoutedEventArgs e)
+        {
+            // beschrijving + foto als tweede box aangevinkt wordt
+            txtDescription.Text = "Tomatensaus, mozzarella, champignons, artisjok, olijven en ham";
+            ImgPizza.Source = new BitmapImage(new Uri("Images/pizza_4_saisons.png", UriKind.Relative));
+        }
+
+        private void rbn1_3_Checked(object sender, RoutedEventArgs e)
+        {
+            // beschrijving + foto als derde box aangevinkt wordt
+            txtDescription.Text = "Tomatensaus; mozzarella, ham en ananas";
+            ImgPizza.Source = new BitmapImage(new Uri("Images/Hawaipng.png", UriKind.Relative));
         }
     }
 }
