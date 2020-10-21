13ee711d532061303028bc878bcb3c88355b96ea
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-21 14:01:10 +0200

    console hms + asci

---
+++ SlnLes03/ConsoleHms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // layout
+            Console.Write("geef het aantal seconden in: ");
+
+            // omzetten van startseconden naar een int
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            // de variabelen uren, minuten en seconden alsook de rest seconden na deling
+            int uren = start / 3600;
+            int rest1 = start % 3600;
+            int minuten = rest1 / 60;
+            int rest2 = rest1 % 60;
+            int seconden = rest2;
+
+            // output met string interpolatie
+            Console.WriteLine($"omgezet in hms formaat: {uren}:{minuten}:{seconden}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ SlnLes03/ConsoleKarakter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKarakter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // werkt nog niet, vind voorlopig nog niet hoe ik dit moet converteren
+            Console.Write("Geef een kleine letter: ");
+            string c = Console.ReadLine();
+            Console.WriteLine(System.Convert.ToInt32(c));
+            
+            //foreach (char c in a)
+            //{
+            //    Console.WriteLine(System.Convert.ToInt32(c));
+            //}
+            Console.ReadKey();
+
+        }
+    }
+}
