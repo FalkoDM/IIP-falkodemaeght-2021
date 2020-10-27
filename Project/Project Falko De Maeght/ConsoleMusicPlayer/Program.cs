cf9021d47c7b50336182300a9a89133e32c6e3a4
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-27 19:00:44 +0100

    update music player

--- Project/Project Falko De Maeght/ConsoleMusicPlayer/Program.cs
+++ Project/Project Falko De Maeght/ConsoleMusicPlayer/Program.cs
@@ -11,20 +11,129 @@ namespace ConsoleMusicPlayer
     {
         static void Main(string[] args)
         {
-           // Layout 
+           // Layout start
             Console.WriteLine("MEDIAPLAYER");
             Console.WriteLine("===========");
             Console.Write("Bestand afspelen: ");
 
-            // ask for song path
+            // geef het pad in naar het liedje
             var song = Console.ReadLine();
+            string keuze = "";
+            int klik = 0;
 
-            // initialize player
+            // initialiseer player
             WindowsMediaPlayer player = new WindowsMediaPlayer();
             player.URL = song;
 
-            // press key to exit player
-            Console.ReadKey();
+
+            do
+            {
+                // keuzemenu Music Player
+                Console.WriteLine("1. Pauze / Play");
+                Console.WriteLine("2. Volume wijzigen");
+                Console.WriteLine("3. Mute / Unmute");
+                Console.WriteLine("3. Nieuw liedje afspelen");
+                Console.WriteLine("4. Stoppen");
+                Console.WriteLine("5. Afsluiten");
+                Console.WriteLine("");
+
+                //geef het huidige volume weer in percentage en in aantal balkjes, gebruik for loop
+                int huidigVolume = player.settings.volume;
+                Console.WriteLine($"Het huidige volume is {huidigVolume}%");
+                Console.WriteLine($"{huidigVolume / 5} balkjes");
+                for (; huidigVolume>=0 && huidigVolume <= 100; huidigVolume -= 5)
+                {
+                    Console.Write("# ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine($" het liedje dat wordt afgespeeld is {song}");
+                Console.WriteLine("");
+                Console.Write("je keuze: ");
+                keuze = Console.ReadLine();
+                
+                Console.WriteLine("");
+
+                    // switch case break methode om de keuze te bepalen en per keuze iets anders uit te voeren
+                switch (keuze)
+                {
+                    // gebruik een klik +1 methode om af te wisselen tussen pauze en play
+                    case "1":
+                        klik += 1;
+                        if (klik == 1)
+                        {
+                        player.controls.pause();
+
+                        }
+                        else
+                        {
+                        player.controls.play();
+                        klik = 0;
+                        }
+                        Console.Clear();
+                        break;
+                    case "2":
+
+                        // gebruik een while loop tot je een correcte waarde voor volume hebt verkregen van de gebruiker
+                        int nieuwVolume;
+                        Console.Write("Geef een nieuw volume: ");
+                        while (!int.TryParse(Console.ReadLine(), out nieuwVolume))
+                        {
+                            Console.WriteLine("Dit is geen correcte waarde voor het volume ");
+                            Console.Write("geef een nieuw volume: ");
+                        }
+                        player.settings.volume = nieuwVolume;
+                        Console.Clear();
+                        break;
+
+                        // gebruik dezelfde methode als case 1 maar dna om te muten / unmuten
+                    case "3":
+                        klik += 1;
+                        if (klik == 1)
+                        {
+                            player.settings.mute = true;
+
+                        }
+                        else
+                        {
+                            player.settings.mute = false;
+                            klik = 0;
+                        }
+                        Console.Clear();
+                        break;
+
+                    case "4":
+
+                        // geef de naam van het nieuwe liedje zonder .mp3
+                        Console.WriteLine("Geef de naam van het nieuwe liedje in");
+                        var newSong = Console.ReadLine();
+                        var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                        player.URL = System.IO.Path.Combine(musicFolder, $"{newSong}.mp3");
+                        song = newSong;
+                        Console.Clear();
+                        break;
+                    case "5":
+
+                        // stop het liedje
+                        player.controls.stop();
+                        Console.Clear();
+                        break;
+                    case "6":
+
+                        // sluit de Music player af
+                        Console.WriteLine("tot ziens");
+                        break;
+
+                        // zorgt ervoor dat de gebruiker een geldige keuze ingeeft
+                    default:
+                        Console.WriteLine("Ongeldige keuze! Druk enter om terug te keren naar het vorige menu");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+            while (keuze != "6");
+            
         }
     }
 }
