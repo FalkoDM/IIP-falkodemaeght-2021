63b28d6249d3118c65e3fd5c9857c2c58801746f
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-28 18:20:40 +0100

    correctie

--- SlnLes06/ConsoleBankautomaat/Program.cs
+++ SlnLes06/ConsoleBankautomaat/Program.cs
@@ -10,23 +10,38 @@ namespace ConsoleBankautomaat
     {
         static void Main(string[] args)
         {
+            // variabelen
+            double saldo = 500;
+            int pogingen = 3;
+            int pincode;
             // layout
             Console.WriteLine("Bankautomaat");
             Console.WriteLine("============");
             Console.WriteLine("");
-            Console.Write("Geef je pincode ");
-            int pincode = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("");
+            Console.Write("Geef je pincode: ");
+            int.TryParse(Console.ReadLine(), out pincode);
 
-            // variabelen
-            string keuze = "";
-            double saldo = 500;
-            double bedrag = 0;
-            int pogingen = 0;
-            
+            // gebruiker heeft drie pogingen om de pin correct in te geven
+            while (pincode != 1234 && pogingen > 0)
+            {
+                pogingen--;
+                if (pogingen > 0)
+                {
+                Console.Write("Geef je pincode: ");
+                int.TryParse(Console.ReadLine(), out pincode);
+                }
+                else
+                {
+                    Console.WriteLine("je account wordt geblokkeerd");
+                    Console.ReadKey();
+                }
+
+
+            }
             // gebruiker geeft pincode in dat moet matchen aan 1234 en komt dan pas in bankmenu terecht
             if (pincode == 1234)
             {
+                string keuze;
                 do
                 {
                     // keuzemenu bankautomaat
@@ -43,7 +58,7 @@ namespace ConsoleBankautomaat
                         // switch case break methode om de keuze te bepalen en per keuze iets anders uit te voeren
                         case "a":
                             Console.Write("Welk bedrag wil je afhalen? ");
-                            bedrag = Convert.ToDouble(Console.ReadLine());
+                            double bedrag = Convert.ToDouble(Console.ReadLine());
                             saldo -= bedrag;
 
                             // saldo mag niet onder nul zakken
@@ -55,6 +70,7 @@ namespace ConsoleBankautomaat
                             else
                             {
                                 Console.WriteLine("saldo is ontoereikend");
+                                saldo += bedrag;
                                 Console.WriteLine("");
                             }
                             break;
@@ -72,10 +88,10 @@ namespace ConsoleBankautomaat
 
                             // uitbreiding pincode veranderen
                             Console.Write("Geef je nieuwe pincode: ");
-                            pincode = Convert.ToInt32(Console.ReadLine()); 
+                            pincode = Convert.ToInt32(Console.ReadLine());
                             Console.WriteLine($"Je nieuwe pincode is {pincode}");
                             Console.WriteLine("");
-                            break;                           
+                            break;
                         default:
                             Console.WriteLine("Ongeldige keuze");
                             Console.WriteLine("");
@@ -83,23 +99,6 @@ namespace ConsoleBankautomaat
                     }
                 }
                 while (keuze != "c");
-                Console.ReadKey();
-            }
-
-            // als de pincode niet overeenkomt
-            else
-            {
-                do
-                {
-
-                    // gebruiker krijgt drie pogingen om de pin juist in te geven of acount wordt geblokkeerd
-                    pogingen++;
-                    Console.Write("geef je pincode ");
-                    pincode = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("");
-                }
-                while (pogingen < 2);
-                Console.WriteLine("Je account wordt geblokkeerd");
                 Console.ReadKey();
             }
         }
--- SlnLes06/ConsoleOnderBoven/Program.cs
+++ SlnLes06/ConsoleOnderBoven/Program.cs
@@ -10,17 +10,89 @@ namespace ConsoleOnderBoven
     {
         static void Main(string[] args)
         {
+            // declareer variabelen ondergrens en bovengrens als integer
+            int ondergrens;
+            int bovengrens;
             Console.Write("Geef een ondergrens: ");
-            int ondergrens = Convert.ToInt32(Console.ReadLine());
+
+            // check of de input van de gebruiker wel een integer is
+            while (!int.TryParse(Console.ReadLine(), out ondergrens))
+            {
+                Console.WriteLine("Dit is geen correcte waarde voor ondergrens");
+                Console.Write("Geef een ondergrens: ");
+            }
+            
             Console.Write("Geef een bovengrens: ");
-            int bovengrens = Convert.ToInt32(Console.ReadLine());
 
+            // check of de input van de gebruiker wel een integer is
+            while (!int.TryParse(Console.ReadLine(), out bovengrens))
+            {
+                Console.WriteLine("Dit is geen correcte waarde voor bovengrens");
+                Console.Write("Geef een bovengrens: ");
+            }
+            do
+            {
+                // als de bovengrens kleinder is dan de ondergrens vraag dan om een nieuwe bovengrens in te geven
+                // check eveneens of de nieuwe input wel een integer is
+                if (bovengrens < ondergrens)
+                {
+                    int nieuweBovengrens;
+                    Console.Write("Bovengrens mag niet kleiner zijn dan ondergrens. Geef een bovengrens: ");
+                    while (!int.TryParse(Console.ReadLine(), out nieuweBovengrens))
+                    {
+                        Console.WriteLine("Dit is geen correcte waarde voor bovengrens");
+                        Console.Write("Geef een nieuwe bovengrens: ");
+                    }
+                    bovengrens = nieuweBovengrens;
+                }
 
+                // we doen dezelfde check maar nu kijken we of de bovengrens gelijk is aan de ondergrens
+                else if (bovengrens == ondergrens)
+                {
+                    int nieuweBovengrens;
+                    Console.Write("Bovengrens mag niet gelijk zijn aan ondergrens. Geef een bovengrens: ");
+                    while (!int.TryParse(Console.ReadLine(), out nieuweBovengrens))
+                    {
+                        Console.WriteLine("Dit is geen correcte waarde voor bovengrens");
+                        Console.Write("Geef een nieuwe bovengrens: ");
+                    }
+                    bovengrens = nieuweBovengrens;
+                }
 
+                // opnieuw hetzelfde maar we kijken of de bovengrens groter is dan 100
+                else if (bovengrens > 100)
+                {
+                    int nieuweBovengrens;
+                    Console.Write("Bovengrens mag niet groter zijn dan 100. Geef een bovengrens: ");
+                    while (!int.TryParse(Console.ReadLine(), out nieuweBovengrens))
+                    {
+                        Console.WriteLine("Dit is geen correcte waarde voor de bovengrens");
+                        Console.Write("Geef een nieuwe bovengrens: ");
+                    }
+                    bovengrens = nieuweBovengrens;
+                }
 
+                // opnieuw hetzelfde maar hier kijken we of de ondergrens niet kleiner is dan 0
+                else if (ondergrens <= 0)
+                {
+                    int nieuweOndergrens;
+                    Console.Write("Ondergrens mag niet kleiner of gelijk zijn aan 0. Geef een ondergrens: ");
+                    while (!int.TryParse(Console.ReadLine(), out nieuweOndergrens))
+                    {
+                        Console.WriteLine("Dit is geen correcte waarde voor de ondergrens");
+                        Console.Write("Geef een nieuwe ondergrens: ");
+                    }
+                    ondergrens = nieuweOndergrens;
+                }
 
+            } 
 
+            // we blijven dit herhalen zolang de onderstaande voorwaarde voldaan zijn
+            while (bovengrens <= ondergrens || bovengrens > 100 || ondergrens <= 0);
 
+            // als alle bovenstaande voorwaarde false zijn geef dan het bereik weer
+            Console.WriteLine($"Bereik: van {ondergrens} tot {bovengrens}");
+            Console.ReadKey();
         }
     }
 }
