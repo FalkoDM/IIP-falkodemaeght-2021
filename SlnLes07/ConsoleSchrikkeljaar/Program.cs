7fd281ae78853576bf4b6f16c168b8d5f18e8afc
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-31 20:48:38 +0100

    console schrikkeljaar

---
+++ SlnLes07/ConsoleSchrikkeljaar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSchrikkeljaar
+{
+    class Program
+    {
+        // methode om na te gaan of het een schrikkeljaar is of niet
+        static private void BerekenSchrikkeljaar(int jaartal)
+        {
+            // als jaartal deelbaar is door vier en niet door 100 maar wel door 400
+            if (jaartal % 4 == 0 && jaartal % 100 != 0 || jaartal % 400 == 0)
+            {
+                Console.WriteLine($"Het jaar {jaartal} is een schrikkeljaar.");
+            }
+            else
+            {
+                Console.WriteLine($"Het jaar {jaartal} is geen schrikkeljaar.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // variabele
+            int jaartal;
+
+            // blijf de gebruiker om een jaartal vragen tot die een ingeeft die nul of lager is dan nul
+            do 
+            {
+                Console.Write("Geef een jaartal: ");
+                int.TryParse(Console.ReadLine(), out jaartal);
+                BerekenSchrikkeljaar(jaartal); // <-- roep de methode op na inlezen getal
+            }
+            while (jaartal > 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SlnLes07/ConsoleSchrikkeljaar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleSchrikkeljaar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleSchrikkeljaar")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad2c9d56-5e3b-45b2-9337-461a64efcb9b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
