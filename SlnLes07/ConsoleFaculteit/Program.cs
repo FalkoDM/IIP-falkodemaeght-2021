b42c1565b19ff3c79a76b718da0de72e29653ec7
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-10-31 17:35:01 +0100

    console faculteit

---
+++ SlnLes07/ConsoleFaculteit/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFaculteit
+{
+    class Program
+    {
+        // methode voor berekenen faculteit
+        static private int Faculteit(int n)
+        {
+            // variabele resultaat
+            int resultaat = 1;
+
+            // zolang het ingegeven getal groter is dan 1 dan is het resultaat * het ingegeven getal trek er eentje af en ga terug rond
+            // het nieuwe resultaat is dan het vorige resultaat * het getal - 1 enzoverder.
+            while (n > 1)
+            {
+                resultaat *= n;
+                n--;
+            }
+
+            // deze methode returned dan het eindresultaat
+            return resultaat;
+    
+        }
+        static void Main(string[] args)
+        {
+
+            // layout
+            Console.WriteLine("FACULTEIT BEREKENEN");
+            Console.WriteLine("");
+
+            // vraag input en zet om naar integer
+            Console.Write("Geef een geheel getal: ");
+            int getal = Convert.ToInt32(Console.ReadLine());
+
+            // roep methode op
+            int resultaat = Faculteit(getal);
+
+            // display het resultaat in de console
+            Console.WriteLine($"de faculteit van {getal} = {resultaat}");
+            Console.ReadKey();
+
+        }
+    }
+}
