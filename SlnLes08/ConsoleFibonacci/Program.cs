077cb8d9090b8cb99e69b2335e37ba341967923a
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-11-10 22:26:55 +0100

    console fibo

---
+++ SlnLes08/ConsoleFibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.Write("Hoeveel Fibonacci getallen wil je afdrukken: ");
+            int range = Convert.ToInt32(Console.ReadLine());
+            
+            // declareer de array
+            int[] array = new int[range];
+
+            // schrijf de lijst uit (string.join dient om een spatie mee te geven)
+            Console.Write($"De eerste {range} Fibonacci getallen zijn: {string.Join(" ", BerekenFibonacci(array, range))}");
+            Console.ReadKey();
+        }
+        // methode voor het berekenen van de fibonacci rij
+        static private int[] BerekenFibonacci(int[] array, int range)
+        {        
+            // startwaarde van de reeks
+            int getal1 = 0;
+            int getal2 = 1;
+            for ( int i = 0; i < range; i++)
+            {
+                int temp = getal1; // sla eerste getal op in een variabele
+                getal1 = getal2;  // switch de getallen
+                getal2 += temp;  // tel ze bij mekaar op
+                array[i] = getal1; // sla ze tot slot op in de array
+            }
+            return array;          
+        }
+    }
+}
