a38246cd373b4a2b8941e6a4facd7bd95328e83a
Falko De Maeght <falko.demaeght@student.odisee.be>
2020-11-13 17:58:09 +0100

    import export uitbreding

--- Project/Project Falko De Maeght/ConsoleMusicPlayer/Program.cs
+++ Project/Project Falko De Maeght/ConsoleMusicPlayer/Program.cs
@@ -2,6 +2,7 @@
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
@@ -20,13 +21,15 @@ namespace ConsoleMusicPlayer
             // roep list op en steek ze in de variabele songs
             var songs = GetSongList();
 
-            // variabele maxValue en maxIndex om de grootst mogelijke index van de lijst te bepalen
-            var maxValue = songs.LastOrDefault();
-            int maxIndex = songs.IndexOf(maxValue);
+            // variabele maxindex en index voor selectie van de liedjes
+            int index;
+            int maxIndex = GetMaxIndex(songs);
 
             // variabele newSong die het huidige liedje bijhoudt en keuzeMediaPlayer die de keuze van de gebruiker bijhoudt
+            // bestand leest de file in die nodig is voor import en export van bestanden
             string newSong = "";
             string keuzeMediaPlayer = "";
+            string bestand;
             do
             {
                 // layout Mediaplayer menu
@@ -34,7 +37,7 @@ namespace ConsoleMusicPlayer
                 PrintArtistEnNummer(newSong);
                 PrintMediaPlayer(player);
 
-                // convert consolekeyinfo naar string keuze en ga verder zonder Enter te duwen
+                // convert consolekeyinfo naar string keuzeMediaPlayer en ga verder zonder Enter te duwen
                 var key = Console.ReadKey();
                 keuzeMediaPlayer = key.KeyChar.ToString();
 
@@ -48,7 +51,7 @@ namespace ConsoleMusicPlayer
                         PrintArtistEnNummer(newSong);
                         PrintPlaylistMenu();
 
-                        // omzetten consolekeyinfo naar string menu en ga verder zonder Enter te duwen
+                        // omzetten consolekeyinfo naar string keuzePlaylist en ga verder zonder Enter te duwen
                         var keyMenu = Console.ReadKey();
                         string keuzePlaylist = keyMenu.KeyChar.ToString();
 
@@ -56,26 +59,55 @@ namespace ConsoleMusicPlayer
                         switch (keuzePlaylist)
                         {
                             case "1":
-                                newSong = PlaySong(player, songs, newSong, maxIndex);
+                                PrintPlaylist(songs);
+                                Console.Write("geef een liedje om af te spelen: ");
+                                index = Convert.ToInt32(Console.ReadLine());
+                                index = ControleerIndex(index, maxIndex); // vraagt nieuwe waarde na te hoge of te lage ingave
+                                newSong = songs[index];
+                                GetSong(newSong, player);
                                 break;
                             case "2":
-                                newSong = PlayNextSong(player, songs, newSong, maxIndex);
+                                newSong = PlayNextSong(songs, newSong, maxIndex);
+                                GetSong(newSong, player);
                                 break;
                             case "3":
-                                newSong = PlayPreviousSong(player, songs, newSong, maxIndex);
+                                newSong = PlayPreviousSong(songs, newSong, maxIndex);
+                                GetSong(newSong, player);
                                 break;
                             case "4":
                                 maxIndex = AddNewSong(songs, maxIndex);
                                 break;
                             case "5":
-                                maxIndex = RemoveSong(songs, maxIndex);
+                                PrintPlaylist(songs);
+                                Console.Write("Geef het nummer van het liedje dat je wilt verwijderen: ");
+                                index = Convert.ToInt32(Console.ReadLine());
+                                maxIndex = RemoveSong(songs, maxIndex, index);
                                 break;
                             case "6":
-                                SwapNummers(songs, maxIndex);
+                                PrintPlaylist(songs);
+                                Console.Write("Geef het nummer dat je van plaats wilt veranderen: ");
+                                int nummer1 = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Geef het nummer naar waar je het wilt verplaatsen: ");
+                                int nummer2 = Convert.ToInt32(Console.ReadLine());
+                                SwapNummers(songs, maxIndex, nummer1, nummer2);
+                                break;
+                            case "7":
+                                Console.WriteLine("Geef bestandsnaam van de muzieklijst: ");
+                                bestand = Console.ReadLine();
+                                ImportMedia(bestand, songs);
+
+                                // maxindex opnieuw berekenen van de gehele lijst
+                                maxIndex = songs.Count()-1;
+                                break;
+                            case "8":
+                                Console.WriteLine();
+                                Console.WriteLine("Geef bestandsnaam aan de muzieklijst: ");
+                                bestand = Console.ReadLine();
+                                ExportMedia(bestand, songs);                              
                                 break;
 
-                            // bij keuze zeven wordt er automatisch teruggekeerd naar het vorige menu
-                            case "7":
+                            // bij keuze negen wordt er automatisch teruggekeerd naar het vorige menu
+                            case "9":
                                 break;
                             default:
                                 Console.WriteLine("Dit is geen geldige keuze");
@@ -86,10 +118,14 @@ namespace ConsoleMusicPlayer
                         PauzePlay(player);
                         break;
                     case "3":
-                        ChangeVolume(player);
+                        Console.WriteLine();
+                        Console.Write("Geef een nieuw volume: ");
+                        int nieuwVolume = Convert.ToInt32(Console.ReadLine());
+                        player.settings.volume = ChangeVolume(nieuwVolume);                
                         break;
                     case "4":
-                        MuteUnmute(player);
+                        // wisselt mute en unmute om afhankelijk van de huidige status
+                        player.settings.mute = !player.settings.mute;
                         break;
                     case "5":
                         player.controls.stop();
@@ -147,17 +183,15 @@ namespace ConsoleMusicPlayer
         // methode voor het afbeelden van de artiest en het nummer
         static private void PrintArtistEnNummer(string newSong)
         {
-            // als er geen liedje geselecteerd is return niets
-            if (newSong == "")
+            // voer onderstaande code enkel uit als er een liedje geselecteerd is 
+            if (newSong != "")
             {
-                return;
+                // split de string aan het "-" teken, beeld het eerste deel van de string af op een lijn en vervolgens het tweede deel op een nieuwe lijn
+                string[] words = newSong.Split('-');
+                Console.WriteLine($"De artiest is: {words[0].ToUpper()} {Environment.NewLine}" +
+                $"Het nummer is:{words[1].ToUpper()}");
+                Console.WriteLine();   
             }  
-            
-           // split de string aan het "-" teken, beeld het eerste deel van de string af op een lijn en vervolgens het tweede deel op een nieuwe lijn
-           string[] words = newSong.Split('-');
-           Console.WriteLine($"De artiest is: {words[0].ToUpper()} {Environment.NewLine}" +
-           $"Het nummer is:{words[1].ToUpper()}");
-           Console.WriteLine();    
         }
 
         //methode om het media player menu af te beelden
@@ -200,7 +234,9 @@ namespace ConsoleMusicPlayer
             Console.WriteLine("4. Voeg liedje toe");
             Console.WriteLine("5. Verwijder Liedje");
             Console.WriteLine("6. Verwissel liedje van plaats");
-            Console.WriteLine("7. Keer terug naar vorig menu");
+            Console.WriteLine("7. Import Media (uit tekstbestand)");
+            Console.WriteLine("8. Export Media (naar tekstbestand)");
+            Console.WriteLine("9. Keer terug naar vorig menu");
             Console.Write("Je keuze: ");
         }
 
@@ -218,14 +254,30 @@ namespace ConsoleMusicPlayer
 
 
         // PLAYLIST METHODES
-        // methode om een positief geheel getal te verkrijgen (index)
-        static private int GetIndex()
+        // bepaalt de maxIndex van de lijst
+        static private int GetMaxIndex(List<string> songs)
         {
-            int index;
-            while (!int.TryParse(Console.ReadLine(), out index) || index < 0)
+            int maxIndex;
+            if (songs.Count() != 0) // als de lijst een item bevat dan is de maxIndex de optelsom van alle items in de lijst -1
+            {
+                maxIndex = songs.Count() - 1;
+            }
+            else
+            {
+                maxIndex = 0; // is de lijst leeg dan is de maxindex sowieso 0
+            }
+            return maxIndex;
+        }
+
+        // methode om een getal te verkrijgen tussen 0 en de maxIndex, zolang de gebruiker een foutief getal ingeeft blijf herhalen
+        static private int ControleerIndex(int index, int maxIndex)
+        {
+            while (index > maxIndex || index < 0)
             {
                 Console.WriteLine("");
-                Console.Write("Geef een correcte waarde: ");
+                Console.WriteLine("Geen geldige keuze! ");
+                Console.Write("geef een nieuwe waarde: ");
+                index = Convert.ToInt32(Console.ReadLine());
             }
             return index;
         }
@@ -237,27 +289,10 @@ namespace ConsoleMusicPlayer
             player.URL = System.IO.Path.Combine(musicFolder, $"{newSong}.mp3");
         }
 
-        // methode om een liedje te selecteren in de playlist en af te spelen
-        static private string PlaySong(WindowsMediaPlayer player, List<string> songs, string newSong, int maxIndex)
-        {
-            PrintPlaylist(songs);
-            Console.Write("geef een liedje om af te spelen: ");
-            int index = GetIndex();
-
-            // als de gebruiker een nummer ingeeft lager of gelijk aan de maxIndex en groter dan 0 speel dan het nummer af 
-            // vermijd dat de music player crashed bij een te hoge of te lage ingave door de gebruiker
-            if (index <= maxIndex && index >= 0)
-            {
-                newSong = songs[index];
-                GetSong(newSong, player);
-            }
-            return newSong;
-        }
-
         // methode om naar het volgende nummer te gaan
-        static private string PlayNextSong(WindowsMediaPlayer player, List<string>songs, string newSong, int maxIndex)
+        static private string PlayNextSong(List<string>songs, string newSong, int maxIndex)
         {
-            // zet de index gelijk aan de index van het huidig liedje
+            // Bepaalt index huidig liedje
             int index = songs.IndexOf(newSong);
 
             // als de index lager is dan de maxIndex en de List songs geen lege lijst is speel dan het volgende nummer af (vermijd crash bij lege lijsten)
@@ -265,25 +300,23 @@ namespace ConsoleMusicPlayer
             {
                 index++;
                 newSong = songs[index];
-                GetSong(newSong, player);
                 return newSong;
             }
 
-            // als de index de maxIndex overschrijdt stel index gelijk aan nul en begin terug van het eerst liedje
+            // als de index de maxIndex overschrijdt stel index gelijk aan nul en begin terug van het eerst liedje in de lijst
             if (index >= maxIndex && songs.Count != 0)
             { 
                 index = 0;
                 newSong = songs[index];
-                GetSong(newSong, player);
                 return newSong;
             }
             return newSong;
         }
 
         // methode om naar het vorige nummer terug te keren
-        static private string PlayPreviousSong(WindowsMediaPlayer player, List<string>songs, string newSong, int maxIndex)
+        static private string PlayPreviousSong(List<string>songs, string newSong, int maxIndex)
         {
-            // stel index gelijk aan index van het huidige nummer
+            // bepaalt index huidig liedje
             int index = songs.IndexOf(newSong);
 
             // als de index groter is dan nul en List songs is geen lege lijst ga dan naar het vorige liedje
@@ -291,7 +324,6 @@ namespace ConsoleMusicPlayer
             {
                 index--;
                 newSong = songs[index];
-                GetSong(newSong, player);
                 return newSong;
             }
             
@@ -300,7 +332,6 @@ namespace ConsoleMusicPlayer
             {
                 index = maxIndex;
                 newSong = songs[index];
-                GetSong(newSong, player);
                 return newSong;
             }
             return newSong;
@@ -322,31 +353,21 @@ namespace ConsoleMusicPlayer
         }
 
         // methode voor het verwijderen van een liedje
-        static private int RemoveSong(List<string>songs, int maxIndex)
+        static private int RemoveSong(List<string>songs, int maxIndex, int index)
         {
-            PrintPlaylist(songs);
-            Console.Write("Geef het nummer van het liedje dat je wilt verwijderen: ");
-            var deleteSongIndex = GetIndex();
-
             // als de lijst niet leeg is en de ingegeven waarde kleiner is dan de maxIndex verwijder dan het liedje
-            if (songs.Count != 0 && deleteSongIndex <= maxIndex)
+            if (songs.Count != 0 && index <= maxIndex)
             {
                 maxIndex--;
-                songs.RemoveAt(deleteSongIndex);   
+                songs.RemoveAt(index);   
             }
             return maxIndex;
         }
 
         // methode om twee liedjes van plaats te verwisselen
-        static private void SwapNummers(List<string> songs, int maxIndex)
+        static private void SwapNummers(List<string> songs, int maxIndex, int nummer1, int nummer2)
         {
-            PrintPlaylist(songs);
-            Console.Write("Geef het nummer dat je van plaats wilt veranderen: ");
-            int nummer1 = GetIndex();
-            Console.Write("Geef het nummer naar waar je het wilt verplaatsen: ");
-            int nummer2 = GetIndex();
-
-            // als de lijst meer dan een liedje bevat is en nummer 1 en 2 zijn lager dan de maxIndex, verwissel ze dan van plaats
+            // als de lijst meer dan 1 liedje bevat en nummer 1 en 2 zijn lager dan de maxIndex, verwissel ze dan van plaats
             if (songs.Count > 1 && nummer1 <= maxIndex && nummer2 <= maxIndex)
             {
                 //  sla 1 van de 2 nummers op in een variabele
@@ -355,25 +376,54 @@ namespace ConsoleMusicPlayer
                 // doe de switch
                 songs[nummer2] = songs[nummer1];
 
-                // stel het eerste nummer gelijk aan de variabele
+                // stel het eerste nummer gelijk aan de tijdelijke variabele
                 songs[nummer1] = temporary;
             }
+        }
+
+        // methode om de playlist te exporteren naar een tekstbestand
+        static private void ExportMedia(string bestand, List<string> songs)
+        {
+            var docsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string doelBestand = System.IO.Path.Combine(docsFolder, $"{bestand}.txt");
+            File.WriteAllLines(doelBestand, songs);
+
+        }
+
+        // methode om de playlist te importeren van een tekstbestand
+        static private void ImportMedia(string bestand, List<string> songs)
+        {
+            var docsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string doelBestand = System.IO.Path.Combine(docsFolder, $"{bestand}.txt");
+
+            // enkel als de file bestaat voer onderstaande code uit https://docs.microsoft.com/en-us/dotnet/api/system.io.file.exists?view=net-5.0
+            if (File.Exists(doelBestand))
+            {
+                using (StreamReader leesTeksBestand = File.OpenText(doelBestand))
+                {
+                    string listLine;
+                    while ((listLine = leesTeksBestand.ReadLine()) != null)
+                    {
+                        songs.Add($"{listLine}");
+                    }
+                }
+            }
+
         }
 
 
         // METHODES MEDIA PLAYER MENU
         // methode om aan de gebruiker een nieuw volume te vragen
-        static private void ChangeVolume(WindowsMediaPlayer player)
+        static private int ChangeVolume(int nieuwVolume)
         {
-            Console.WriteLine();
-            int nieuwVolume;
-            Console.Write("Geef een nieuw volume: ");
-            while (!int.TryParse(Console.ReadLine(), out nieuwVolume) || nieuwVolume > 100 || nieuwVolume < 0)
+
+            while (nieuwVolume > 100 || nieuwVolume < 0)
             {
                 Console.WriteLine("Dit is geen correcte waarde voor het volume ");
                 Console.Write("geef een nieuw volume: ");
+                nieuwVolume = Convert.ToInt32(Console.ReadLine());
             }
-            player.settings.volume = nieuwVolume;
+            return nieuwVolume;
         }
         
         // methode om te wisselen tussen pauze en play
@@ -390,12 +440,6 @@ namespace ConsoleMusicPlayer
                 player.controls.play();
                 
             }
-        }
-        
-        // methode om te wisselen tussen mute en unmute
-        static private void MuteUnmute(WindowsMediaPlayer player)
-        {
-            player.settings.mute = !player.settings.mute;
         }
     }
 }
